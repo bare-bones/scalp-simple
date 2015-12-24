c73ac69f0ca370bbaef6285947866e25b8f71f10
Ben Monro <ben.monro@gmail.com>
2015-12-24 13:22:14 -0800

    scalp config example

---
+++ scalp.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "my-app-variable": {
+        "prompt": "please enter a variable for the app name"
+    },
+    "my-app-name": {
+        "prompt": "what's the name of the application?"
+    }
+};
