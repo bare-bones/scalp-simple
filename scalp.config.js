d8790b845168421e47686f957383c7f17cc72506
Ben Monro <ben.monro@gmail.com>
2015-12-24 15:57:39 -0800

    default value

--- scalp.config.js
+++ scalp.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     "my_variable_name": {
-        "prompt": "please enter a variable for the app name"
+        "prompt": "please enter a variable for the app name",
+        "defaultValue": "123"
     },
     "application_name": {
         "prompt": "what's the name of the application?"
